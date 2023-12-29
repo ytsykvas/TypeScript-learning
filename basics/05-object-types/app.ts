8648c5e869f7aeaccb978ac7c3c28d75e7e592bb
Yurii Tsykvas <tsykvasyurii@gmail.com>
2023-12-29 18:59:21 +0200

    Basics with types

---
+++ basics/05-object-types/app.ts
@@ -0,0 +1,11 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: 'Yurii Tsykvas',
+  age: 28
+};
+
+console.log(person.name);
+console.log(person.age);
